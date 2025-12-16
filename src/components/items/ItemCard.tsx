ca9a6ab56f918104632c61664ee52820a37e5c11
Pmelinte <pmelinte@gmail.com>
2025-12-16 18:00:43 +0200

    Add alias export for ItemCard component
    Create an alias for ItemCard component.

---
+++ src/components/items/ItemCard.tsx
@@ -0,0 +1,5 @@
+// src/components/items/ItemCard.tsx
+// Bridge: păstrăm codul sursă în /components/items/ItemCard.tsx
+// și expunem același modul prin aliasul "@/components/..."
+
+export { default } from "../../../components/items/ItemCard";
