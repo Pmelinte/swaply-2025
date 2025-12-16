2b27ffdfd93afdba8a40534bed67b973641151b9
Pmelinte <pmelinte@gmail.com>
2025-12-16 18:06:00 +0200

    Create AppHeader.tsx for component imports

---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,6 @@
+// src/components/AppHeader.tsx
+// Bridge pentru importurile "@/components/AppHeader" după ce @/ -> src/*
+// Sursa de adevăr rămâne în /components/AppHeader.tsx
+
+export { default } from "../../components/AppHeader";
+export * from "../../components/AppHeader";
