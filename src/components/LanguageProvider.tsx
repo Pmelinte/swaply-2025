c196f69d524bf5b734de7b9d16029a1f12b28447
Pmelinte <pmelinte@gmail.com>
2025-12-16 18:03:08 +0200

    Create LanguageProvider.tsx for component imports

---
+++ src/components/LanguageProvider.tsx
@@ -0,0 +1,6 @@
+// src/components/LanguageProvider.tsx
+// Bridge pentru importurile "@/components/LanguageProvider" după ce @/ -> src/*
+// Sursa de adevăr rămâne în /components/LanguageProvider.tsx
+
+export { default } from "../../components/LanguageProvider";
+export * from "../../components/LanguageProvider";
