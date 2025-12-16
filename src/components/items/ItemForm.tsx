41871f3944f10ecfdb9abdb766705e79ad0f5236
Pmelinte <pmelinte@gmail.com>
2025-12-16 18:00:04 +0200

    Create ItemForm.tsx to export item form module

---
+++ src/components/items/ItemForm.tsx
@@ -0,0 +1,5 @@
+// src/components/items/ItemForm.tsx
+// Bridge: păstrăm codul sursă în /components/items/ItemForm.tsx
+// și expunem același modul prin aliasul "@/components/..."
+
+export { default } from "../../../components/items/ItemForm";
