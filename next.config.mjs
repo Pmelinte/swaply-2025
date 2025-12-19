caacb25bb860280202f5944950cc4577f172dda5
Pmelinte <pmelinte@gmail.com>
2025-12-20 01:21:14 +0200

    change next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Strict mode poate fi reactivat când stabilizăm complet client/server boundaries.
-  reactStrictMode: false,
-
-  // Ca să nu-ți mai pice build-ul pe Vercel din warnings ESLint.
-  // (În dev tot ai lint dacă rulezi `next lint`.)
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.cloudinary.com",
+        hostname: "**",
       },
     ],
-  },
-
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "5mb",
-    },
   },
 };
 
